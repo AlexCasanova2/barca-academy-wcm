e7a48b6cfd11e41e442b896ddff297af37e009c8
Alex <casanovaalex11@gmail.com>
2022-03-30 17:43:28 +0200

    update img logo

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -32,7 +32,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="{{url('/admin')}}"><img class="" src="{{url('')}}/packages/images/logo.jpg" /></a>
+				<a class="navbar-brand" href="{{url('/admin')}}"><img class="" src="{{url('')}}/packages/images/logo.jpg" style="width: 13%;"/></a>
 			</div>
 			<div id="navbar" class="collapse navbar-collapse">
 				<ul class="nav navbar-nav">
